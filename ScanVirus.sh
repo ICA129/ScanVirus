76663116b62f623880645009e26d054ffd9f8c9b
ICA129 <51882241+ICA129@users.noreply.github.com>
2020-03-04 20:31:59 +0700

    Create ScanVirus.sh

---
+++ ScanVirus.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "============================================"
+echo "=                                          ="
+echo "=       Scanning Virus & Delete Virus      ="
+echo "=          Coded By Mr.Hmei24/Ham          ="
+echo "=          Islamic Cyber Attacker          ="
+echo "=                                          ="
+echo "============================================"
+echo " "
+echo " "
+echo "=================================================="
+echo "Mulai memindai....."
+echo ""
+find . -type f -name "autorun.inf" -exec rm -fv {} \;
+echo " "
+find . -type f -name "Autorun.inf" -exec rm -fv {} \;
+echo ""
+find . -type f -name "desktop.ini" -exec rm -fv {} \;
+echo ""
+find . -type f -name "dekstop.ini" -exec rm -fv {} \;
+echo ""
+find . -type f -name "*.vbs" -exec rm -fv {} \;
+echo ""
+find . -type f -name "*.vbe" -exec rm -fv {} \;
+echo ""
+find . -type f -name "*.lnk" -exec rm -fv {} \;
+echo ""
+find . -type f -name "*.vbe" -exec rm -fv {} \;
+echo ""
+echo " "
+echo "Selesai.."
+echo "Udah Woi,Pergi Sono, Ga Usah Balik Hehe"
+echo "==================================================="
+echo " "
+echo "Catatan :"
+echo "Apabila terdapat Virus atau Junk File maka akan otomatis di hapus"
+echo " "
+echo "================================="
+echo "=    Copyright© Mr.Hmei24 2019  ="          ="
+echo "================================="
